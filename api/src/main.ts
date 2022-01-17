6a5126038b9dd3813f42874eb76ac8fa282d8abd
Masanori <nitaka.masa@gmail.com>
2022-01-17 18:23:15 +0900

    コミット忘れ

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 import helmet from 'helmet'
 import { HttpExceptionFilter } from './fillter/http-exception.filter';
 import { logger } from './middleware/logger.middleware';
-import csurf from 'csurf';
-import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});

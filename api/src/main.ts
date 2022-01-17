b6f489cc52ee64df42155e3a3f27ba6a62b77f7a
Masanori <nitaka.masa@gmail.com>
2022-01-17 18:09:35 +0900

    一旦CSRFは削除

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,17 +5,15 @@ import helmet from 'helmet'
 import { HttpExceptionFilter } from './fillter/http-exception.filter';
 import { logger } from './middleware/logger.middleware';
 import csurf from 'csurf';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
 
   await app
     .use(logger)
-    .useGlobalPipes(new ValidationPipe({
-      
-    }))
+    .useGlobalPipes(new ValidationPipe())
     .use(helmet())
-    // .use(csurf())
     .useGlobalFilters(new HttpExceptionFilter())
     .listen(3000);
 }
